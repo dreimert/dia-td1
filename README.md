a75583ea070fae70428611970bad04d8ff56622f
Damien Reimert <damien.reimert@gmail.com>
2018-01-23 17:15:38 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# dia-td1
-TD1 du cours de développement et déploiement d'applications distribuées
+# Dia : TD1
+
+TD1 du cours de développement et déploiement d'applications distribuées.
+
+L'objectif de ce TD est de développer un serveur TCP minimaliste répondant à des commandes textes en utilisant Node.js.
+
+## Protocole
+
+Le protocole est en texte et il utilise le port 8123 en TCP. C'est un cache mémoire sur un modèle clé / valeur qui propose les commandes suivantes :
+
+* `KEYS\n` : liste les clés, elles sont séparées par un `\n`;
+* `PUT <key> <data-length> <data>\n` : stocke une chaine de caractères `<data>` de taille `<data-length>`dans la clé `<key>`. Renvoie au client `ok\n` en cas de succès ou `ko\n` sinon;
+* `GET <key>\n` : pour la clé `<key>` renvoie au client la longueur de la chaine de caractères associé suivie d’un saut de ligne puis la chaine associé. Renvoie `0\n` si la clé est inexistante;
+* `DEL <key>\n` : supprime la clé / valeur `<key>`. Renvoie au client `ok\n` en cas de succès ou `ko\n` sinon.
+
+## Implémentation
+
+En Node.js, la librairie standard ***net*** permet de manipuler facilement les sockets : https://nodejs.org/api/net.html.
+
+Vous pouvez implémenter votre serveur dans le fichier `server.js`.
+
+J'ai configuré pour vous ce dépôt pour que les modifications que vous faites dans le fichier `server.js` recharge automatiquement le serveur. Dans un terminal, lancer les commandes suivantes :
+
+    npm install
+    npm start
+
+`npm install` va installer les dépendances listées dans le `package.json`. Dans notre cas, `nodemon` qui permet de recharger un fichier quand il est modifié.
+
+`npm start` lance la commande `start` définie dans le `package.json`, ici `nodemon server.js`.
+
+## Test
+
+Vous pouvez tester directement via :
+
+    telnet localhost 8123
+
+Ou en utilisant des tests déjà écrits par moi-même :
+
+    npm test
---
+++ client.js
@@ -0,0 +1,102 @@
+const net = require('net');
+
+const createClient = () => {
+  return new Promise((resolve, reject) => {
+    const client = net.createConnection({ port: 8123 }, () => {
+      console.log('1) connected to server!');
+      resolve(client);
+    });
+
+    client.on('error', (err) => {
+      console.error(err.toString());
+      if (err.code === 'ECONNREFUSED') {
+        console.info("Impossible d'établir la connexion sur le port " + err.port + ".");
+        console.info("Est-ce le bon port ? Votre serveur fonctionne t'il ?");
+      }
+      client.end();
+      reject(err);
+    });
+
+    client.on('end', () => {
+      console.log('disconnected from server');
+    });
+  })
+}
+
+let client;
+
+const write = (command) => () => {
+  client.write(command + '\n');
+}
+
+const read = (func) => () => {
+  return new Promise((resolve, reject) => {
+    client.once('data', (data) => {
+      console.log(data.toString());
+      func(data, resolve, reject);
+    });
+  })
+}
+
+const close = () => {
+  client.end();
+}
+
+createClient()
+.then((cli) => {
+  client = cli;
+})
+.then(write('PUT bonjour 5 hello'))
+.then(read((data, resolve, reject) => {
+  if (data.toString() === "ok\n") {
+    console.info("2) PUT success")
+    resolve("2) PUT success")
+  } else {
+    console.error("2) PUT fail")
+    reject("2) PUT fail")
+  }
+}))
+.then(write('KEYS'))
+.then(read((data, resolve, reject) => {
+  if (data.toString() === "bonjour\n") {
+    console.info("3) KEYS success")
+    resolve("3) KEYS success")
+  } else {
+    console.error("3) KEYS fail")
+    reject("3) KEYS fail")
+  }
+}))
+.then(write('GET bonjour'))
+.then(read((data, resolve, reject) => {
+  if (data.toString() === "hello\n") {
+    console.info("4) GET success")
+    resolve("4) GET success")
+  } else {
+    console.error("4) GET fail")
+    reject("4) GET fail")
+  }
+}))
+.then(write('DEL bonjour'))
+.then(read((data, resolve, reject) => {
+  if (data.toString() === "ok\n") {
+    console.info("5) DEL success")
+    resolve("5) DEL success")
+  } else {
+    console.error("5) DEL fail")
+    reject("5) DEL fail")
+  }
+}))
+.then(write('GET bonjour'))
+.then(read((data, resolve, reject) => {
+  if (data.toString() === "0\n") {
+    console.info("6) GET success")
+    resolve("6) GET success")
+  } else {
+    console.error("6) GET fail")
+    reject("6) GET fail")
+  }
+}))
+.catch((err) => {
+  console.error("ERROR: ", err);
+})
+.then(close)
